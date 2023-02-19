6153fd28364e1b0616de3eea355eacb9addd5739
vdtvt <stanislawgzielinski@gmail.com>
2023-02-19 23:09:40 +0100

    Adding Banners

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+const screeningDay = 21;
+const filmDurationHours = 1;
+const filmDurationsMinutes = 28;
+const screeningStartHour = 7;
+
 
 const timeElement = document.querySelector(".time");
 const dateElement = document.querySelector(".date");
+
 /**  
 *@param  {Date} date
 */
@@ -18,10 +24,33 @@ function formatTime(date) {
     const hours12 = date.getHours() % 12 || 12;
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
-    const isAM = date.getHours() < 12;
+    const AM = date.getHours() < 12;
 
-    return `${hours12.toString().padStart(2,"0")}:${minutes.toString().padStart(2,"0")} ${isAM? "AM" : "PM"}`;
+    return `${hours12.toString().padStart(2,"0")}:${minutes.toString().padStart(2,"0")} ${AM? "AM" : "PM"}`;
+}
 
+function displayBanner(date) {
+
+    const hours12 = date.getHours() % 12 || 12;
+    const minutes = date.getMinutes();
+    const AM = date.getHours() < 12;
+    const day = date.getDate();
+
+    const screeningEndHour = screeningStartHour +filmDurationHours;
+    const screeningEndMinute = filmDurationsMinutes;
+
+    // Screening Day blinking banner 
+    const screeningDayToday = day===screeningDay && hours12<screeningStartHour;
+    document.getElementById('blinking-banner-day').style.display = screeningDayToday? "flex" : "none";
+
+    // Thanks Banner when the film ends
+    const filmEnded = day===screeningDay && ((hours12===screeningEndHour && minutes > screeningEndMinute)||hours12 > screeningEndHour);
+    const thanksBannerDays = day - screeningDay >= 0 && day - screeningDay <= 3;
+    document.getElementById('thanks-banner').style.display =  thanksBannerDays && filmEnded ? "flex" : "none"; 
+
+    // Screening Now blinking banner 
+    const screeningNow = day===screeningDay && hours12 >=  screeningStartHour && !filmEnded;
+    document.getElementById('blinking-banner-now').style.display = screeningNow? "flex" : "none";
 }
 
 
@@ -29,9 +58,10 @@ setInterval(() => {
 const now = new Date();
 dateElement.textContent = formatDate(now);
 timeElement.textContent = formatTime(now);
+displayBanner(now);
 }, 500);
 
-document.getElementById("screening-now").style.display="none"
+
 
 //DAYS      const DAYS = ["Sunday", "Monday", "Tuesday","Wednesday", "Thursday", "Friday", "Saturday" ];
 //DAYS      ${DAYS[date.getDay()]},
